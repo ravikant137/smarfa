1ffed197509c880b778385b8dd071e30143ed78d
Your Name <ravikantpatil137@gmail.com>
2026-06-05 13:12:15 +0530

    Use absolute URL instead of relative path to fix fetch resolution on some mobile browsers

--- mobile-app/utils/api.js
+++ mobile-app/utils/api.js
@@ -7,7 +7,7 @@ export const getApiBaseUrl = () => {
     if (typeof window !== 'undefined' && 
         window.location.hostname !== 'localhost' && 
         window.location.hostname !== '127.0.0.1') {
-      return '';
+      return window.location.origin;
     }
     return 'http://127.0.0.1:8000';
   }
